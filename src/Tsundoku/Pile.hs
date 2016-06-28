b2a8a2eda9546a4f1c61f5011e3684b21a48a2e4
Isaac Azuelos <isaac@azuelos.ca>
2016-06-28 13:31:59 -0600

    Passes pile tests

--- src/Tsundoku/Pile.hs
+++ src/Tsundoku/Pile.hs
@@ -17,7 +17,7 @@ module Tsundoku.Pile
 
 import qualified Tsundoku.Book as Book
 
-data Pile = Pile deriving (Show, Eq)
+data Pile = Pile [Book.Book] deriving (Show, Eq)
 
 data PileError
   = NoSuchBookError
@@ -25,16 +25,29 @@ data PileError
     deriving (Show, Eq)
 
 empty :: Pile
-empty = error "empty unimplemented"
+empty = Pile []
 
 list :: Pile -> [Book.Book]
-list = error "list unimplemented"
+list (Pile books) = books
 
 add :: Book.Book -> Pile -> Either PileError Pile
-add = error "add unimplemented"
+add book pile@(Pile books) =
+  case find (Book.title book) pile of
+    Left NoSuchBookError -> Right (Pile (book:books))
+    _ -> Left BookNotUniqueError
 
 delete :: Book.Title -> Pile -> Either PileError Pile
-delete = error "delete unimplemented"
+delete title pile@(Pile books) =
+  case find title pile of
+    Right _ -> Right $ Pile (filter (not . (`hasTitle` title)) books)
+    Left err -> Left err
+
+hasTitle :: Book.Book -> Book.Title -> Bool
+hasTitle book title = Book.title book == title
 
 find :: Book.Title -> Pile -> Either PileError Book.Book
-find = error "fine unimplemented"
+find title pile@(Pile books) =
+  case filter (`hasTitle` title) books of
+    []     -> Left NoSuchBookError
+    [book] -> Right book
+    _      -> Left BookNotUniqueError
